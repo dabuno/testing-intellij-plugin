80cc4fc85f4d5c7ba6c0a8dc3e147d2882d402cf
GitHub Action <action@github.com>
2021-02-17 10:27:14 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/dabuno/testingintellijplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.dabuno.testingintellijplugin.services
+
+import com.github.dabuno.testingintellijplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/dabuno/testingintellijplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/dabuno/testingintellijplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.dabuno.testingintellijplugin.services
 
+import com.github.dabuno.testingintellijplugin.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
